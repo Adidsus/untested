f042ad383969b9dfaa018a176b4b67a8b13f3954
Adidsus <85895319+Adidsus@users.noreply.github.com>
2023-09-03 02:11:48 +0200

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,3 @@
+if game.PlaceId == 13127800756 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Adidsus/Qquae/main/arm/AWS.lua?token=GHSAT0AAAAAACFV6SIFOVLGM6Y6GWRSQHPUZHT3KGQ"))()
+    end
