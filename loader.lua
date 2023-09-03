904739f15397b2f6cd2f17d8f7d0d872b671e7e8
Adidsus <85895319+Adidsus@users.noreply.github.com>
2023-09-03 02:36:46 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 13127800756 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Adidsus/Qquae/main/arm/AWS.lua?token=GHSAT0AAAAAACFV6SIFOVLGM6Y6GWRSQHPUZHT3KGQ"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Adidsus/Qquae/main/arm/AWS.lua?token=GHSAT0AAAAAACFV6SIF47DU2LR2KFQRQBA6ZHT4MCQ"))()
     end
