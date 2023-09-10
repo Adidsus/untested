74f0a2ef41c522890bdb1092157ab3fc81b3b04e
Adidsus <85895319+Adidsus@users.noreply.github.com>
2023-09-10 22:50:54 +0200

    Update loader.lua
    a

--- loader.lua
+++ loader.lua
@@ -1,3 +1,4 @@
-if game.PlaceId == 13127800756 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Adidsus/Qquae/main/arm/AWS.lua?token=GHSAT0AAAAAACFV6SIEQMSFJG7JNVLPT4Q4ZHVEL2A"))()
-    end
+-- if game.PlaceId == 13127800756 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Adidsus/untested/main/petsimx"))()
+    -- end
+-- 
