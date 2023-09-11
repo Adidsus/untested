ad55634334413a6a13dcc8374efdb6575cd2efef
Adidsus <85895319+Adidsus@users.noreply.github.com>
2023-09-11 15:01:34 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Adidsus/Qquae/main/petsimx.lua?token=GHSAT0AAAAAACFV6SIEMPJ775B2OMUYVB7YZH7DWIQ"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Adidsus/untested/main/petsimx.lua"))()
 
 --logs first player execute
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Adidsus/aaa3/main/webhook.lua?token=GHSAT0AAAAAACFV6SIFCMRVGINCBFCDIMU4ZH7C2EQ"))()
